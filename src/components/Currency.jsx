33cb2d00c7f74b190f5dacb2009e84e07e8c7cbb
Batuhan024 <batuhantest2001@gmail.com>
2024-11-26 21:46:04 +0300

    last version

--- src/components/Currency.jsx
+++ src/components/Currency.jsx
@@ -1,30 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../css/currency.css'
 import { FaRegArrowAltCircleRight } from 'react-icons/fa'
+import axios from 'axios';
+
+let BASE_URL = 'https://api.freecurrencyapi.com/v1/latest';
+let API_KEY = 'fca_live_1ohnv1EUvIYJbKb2Abmi6Y2d8zVvpFPnFDcOAMwb';
+
 
 function Currency() {
+
+  const[amount, setAmount] = useState();
+  const[fromCurrency, setFromCurrency] = useState('USD');
+  const[toCurrency, setToCurrency] = useState('TRY');
+  const [result, setResult] = useState(0);
+  
+
+  const exchange = async () => {
+   const response = await axios.get(`${BASE_URL}?apikey=${API_KEY}&base_currency=${fromCurrency}`);
+   const result = (response.data.data[toCurrency]*amount).toFixed(2);
+   setResult(result);
+  }
+
+
   return (
     <div className="currency-div">
     <div>
       <h3>DÖVİZ KURU UYGULAMASI</h3>
     </div>
     <div>
-      <input type="number" className="amount" placeholder="Miktar" />
-      <select className="from-currency-option">
+      <input value={amount} onChange={(e)=> setAmount(e.target.value)} type="number" className="amount" placeholder="Miktar" />
+      <select onChange={(e)=> setFromCurrency(e.target.value)} className="from-currency-option">
         <option>USD</option>
         <option>EUR</option>
-        <option>TL</option>
+        <option>TRY</option>
       </select>
       <FaRegArrowAltCircleRight />
-      <select className="to-currency-option">
-        <option>TL</option>
+      <select onChange={(e)=> setToCurrency(e.target.value)} className="to-currency-option">
+        <option>TRY</option>
         <option>USD</option>
         <option>EUR</option>
       </select>
-      <input type="number" className="result" placeholder="Sonuç" />
+      <input value={result} onChange={(e)=> setResult(e.target.value)} type="number" className="result" placeholder="Sonuç" />
     </div>
     <div>
-      <button className="exchange-button">Çevir</button>
+      <button onClick={exchange} className="exchange-button">Çevir</button>
     </div>
   </div>
   )
